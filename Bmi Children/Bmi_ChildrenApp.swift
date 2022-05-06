8b64544fdda55c354a296e51cdb36feef96320b0
CNLPY <calpay@yahoo.com>
2022-05-06 14:27:37 +0300

    Initial Commit

---
+++ Bmi Children/Bmi_ChildrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bmi_ChildrenApp.swift
+//  Bmi Children
+//
+//  Created by Can on 6.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Bmi_ChildrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
